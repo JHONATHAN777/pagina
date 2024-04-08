fbb988da6031ec2830f8e3625a9fa266cae232f0
JHONATHAN-9507 <jhonatan.perea@innovating.com.co>
2024-04-08 17:12:08 -0500

    visual login users

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,47 +1,68 @@
-<x-guest-layout>
-    <!-- Session Status -->
-    <x-auth-session-status class="mb-4" :status="session('status')" />
+@extends('layouts.form')
 
-    <form method="POST" action="{{ route('login') }}">
-        @csrf
+@section('title', 'Inicia sesión')
 
-        <!-- Email Address -->
-        <div>
-            <x-input-label for="email" :value="__('Email')" />
-            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus autocomplete="username" />
-            <x-input-error :messages="$errors->get('email')" class="mt-2" />
-        </div>
-
-        <!-- Password -->
-        <div class="mt-4">
-            <x-input-label for="password" :value="__('Password')" />
-
-            <x-text-input id="password" class="block mt-1 w-full"
-                            type="password"
-                            name="password"
-                            required autocomplete="current-password" />
-
-            <x-input-error :messages="$errors->get('password')" class="mt-2" />
-        </div>
-
-        <!-- Remember Me -->
-        <div class="block mt-4">
-            <label for="remember_me" class="inline-flex items-center">
-                <input id="remember_me" type="checkbox" class="rounded dark:bg-gray-900 border-gray-300 dark:border-gray-700 text-indigo-600 shadow-sm focus:ring-indigo-500 dark:focus:ring-indigo-600 dark:focus:ring-offset-gray-800" name="remember">
-                <span class="ms-2 text-sm text-gray-600 dark:text-gray-400">{{ __('Remember me') }}</span>
-            </label>
-        </div>
-
-        <div class="flex items-center justify-end mt-4">
-            @if (Route::has('password.request'))
-                <a class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800" href="{{ route('password.request') }}">
-                    {{ __('Forgot your password?') }}
-                </a>
+@section('content')
+<div class="container mt--8 pb-5">
+    <div class="row justify-content-center">
+      <div class="col-lg-5 col-md-7">
+        <div class="card bg-secondary shadow border-0">
+          <div class="card-body px-lg-5 py-lg-5">
+            @if ($errors->any())
+            <div class="text-center text-muted mb-2">
+                <h4>Hay un error</h4>
+              </div>
+            <div class="alert alert-danger mb-4" role="alert">
+                 {{$errors->first()}}
+            </div>
+            @else
+            <div class="text-center text-muted mb-4">
+              <small>Ingresa tus credencial para ingresar al sistema</small>
+            </div>
             @endif
 
-            <x-primary-button class="ms-3">
-                {{ __('Log in') }}
-            </x-primary-button>
+            <form role="form" method="POST" action="{{ route('login') }}">
+                @csrf
+              <div class="form-group mb-3">
+                <div class="input-group input-group-alternative">
+                  <div class="input-group-prepend">
+                    <span class="input-group-text"><i class="ni ni-email-83"></i></span>
+                  </div>
+                  <input class="form-control" placeholder="Ingresa tu correo electronico" type="email" name="email" :value="old('email')" required autofocus autocomplete="username">
+                </div>
+              </div>
+              <div class="form-group">
+                <div class="input-group input-group-alternative" >
+                  <div class="input-group-prepend">
+                    <span class="input-group-text"><i class="ni ni-lock-circle-open"></i></span>
+                  </div>
+                  <input class="form-control" placeholder="Ingresa tu contraseña" type="password"  name="password"
+                  required autocomplete="current-password" >
+                </div>
+              </div>
+              <div class="custom-control custom-control-alternative custom-checkbox">
+                <input name="remember" class="custom-control-input" id="remember" type="checkbox"
+                id="remember_me" type="checkbox" name="remember">
+                <label class="custom-control-label" for="remember">
+                  <span class="text-muted">Recordar sesión{{ __('Remember me') }}</span>
+                </label>
+              </div>
+              <div class="text-center">
+                <button type="submit" class="btn btn-primary my-4">iniciar</button>
+              </div>
+            </form>
+          </div>
         </div>
-    </form>
-</x-guest-layout>
+        <div class="row mt-3">
+          <div class="col-6">
+            <a href="{{ route('password.request') }}"" class="text-light"><small>¿Olvidaste tu contraseña?</small></a>
+          </div>
+          <div class="col-6 text-right">
+            <a href="{{route('register')}}" class="text-light"><small>Registrate</small></a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+@endsection
+
